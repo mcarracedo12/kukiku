b5c3b6b9c9f881dc3ef7a20b286e91d12e6dc327
unknown <marinacarracedo14@gmail.com>
2026-07-23 23:35:25 -0300

    .env added and gitignored

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 // src/services/api.js
 
-const API_BASE_URL = 'http://localhost:5000/api'
+// const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_URL
+
 
 // Función auxiliar para obtener el token desde localStorage
 const getAuthHeaders = () => {
